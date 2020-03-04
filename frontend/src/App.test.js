ebfa683cbd181cc1dbbbc8ce1555e6304fd94480
basselalsayed <alsayed.bassel@gmail.com>
2020-03-04 11:25:05 +0000

    jest snapshot testing working

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const { getByText } = render(<App />);
+  // // const linkElement = getByText(/learn react/i);
+  // expect('hello').toBeInTheDocument();
+
+  const component = renderer.create(
+    <App />,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
