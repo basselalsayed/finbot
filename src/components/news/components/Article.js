9bf2db787dd371bcf5864c5adb7e752747f7a9ee
basselalsayed <alsayed.bassel@gmail.com>
2020-03-11 08:01:55 +0000

    refactored news article

--- src/components/news/components/Article.js
+++ src/components/news/components/Article.js
@@ -1,23 +1,34 @@
-import React, { Component } from 'react'
-import { Card } from 'react-bootstrap'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import { Card, Image } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 export default class Article extends Component {
-
-
   render() {
-    const { story } = this.props
-    return (
-      <Card className='article'>
-        {/* <Card.Img src={story.urlToImage}></Card.Img>
-        <Card.Body> */}
-          <Card.Link className='article-link' href={story.url}>{story.title}</Card.Link>
-        {/* </Card.Body> */}
-      </Card>
-    )
+    const { story } = this.props;
+    if (story.urlToImage !== '') {
+      return (
+        <Card className="article">
+          <Image src={story.urlToImage} thumbnail width="128" height="128" />
+          {/* <Card.Body> */}
+          <Card.Link className="article-link" href={story.url}>
+            {story.title}
+          </Card.Link>
+          {/* </Card.Body> */}
+        </Card>
+      );
+    } else {
+      return (
+        <Card className="article">
+          <Card.Link className="article-link" href={story.url}>
+            {story.title}
+          </Card.Link>
+          {/* </Card.Body> */}
+        </Card>
+      );
+    }
   }
 }
 
 Article.propTypes = {
-  story: PropTypes.object.isRequired
-}
+  story: PropTypes.object.isRequired,
+};
--- src/components/news/components/Container.js
+++ src/components/news/components/Container.js
@@ -1,18 +1,14 @@
-import React, { Component, Fragment } from 'react'
-import { Card } from 'react-bootstrap'
-import Articles from './Articles'
+import React, { Component, Fragment } from 'react';
+import { Card } from 'react-bootstrap';
+import Articles from './Articles';
 
 export default class Container extends Component {
   render() {
     return (
-      <Fragment id='news-api'>
-        <Card.Header> 
-          Financial News 
-        </Card.Header>
+      <Fragment id="news-api">
+        <Card.Header>Financial News</Card.Header>
         <Articles />
       </Fragment>
-    )
+    );
   }
 }
-
-
