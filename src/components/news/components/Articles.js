4d9a4056cb638da3b7d4e9222fccecdec9709fe5
basselalsayed <alsayed.bassel@gmail.com>
2020-03-13 15:10:29 +0000

    updated to point to remote backend 4

--- src/components/news/components/Articles.js
+++ src/components/news/components/Articles.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react'
-import Article from './Article'
-import axios from 'axios'
-import * as uuid from 'uuid'
+import React, { Component } from 'react';
+import Article from './Article';
+import axios from 'axios';
+import * as uuid from 'uuid';
 
 export default class Articles extends Component {
-
   state = {
-    stories: []
-  }
+    stories: [],
+  };
 
   componentDidMount() {
     this.apiCallGet();
   }
 
   apiCallGet = () => {
-    const url = 'http://newsapi.org/v2/top-headlines?country=gb&category=business&' +
-                'apiKey=c883f717ea02425f9adda874534fee3f';
-    axios
-      .get(url)
-      .then(res => this.setState({ stories: res.data.articles }))
+    const url =
+      'https://newsapi.org/v2/top-headlines?country=gb&category=business&' +
+      'apiKey=c883f717ea02425f9adda874534fee3f';
+    axios.get(url).then(res => this.setState({ stories: res.data.articles }));
   };
 
   render() {
-    return this.state.stories.slice(0, 5).map((story) => (
-        <Article key={uuid.v4()} story={story} />
-    ))
+    return this.state.stories
+      .slice(0, 5)
+      .map(story => <Article key={uuid.v4()} story={story} />);
   }
 }
-
